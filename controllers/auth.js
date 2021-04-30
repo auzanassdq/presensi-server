abfa209e74e4579d303c31313ab908aedcfadcfb
auzanassdq <auzanassidqi@gmail.com>
2021-04-30 13:33:52 +0700

    login and regis mahasiswa

---
+++ controllers/auth.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {Mahasiswa} = require('../models');
+
+module.exports = {
+  login: async (req, res, next) => {
+    try {
+      let data = req.body
+      let user = await Mahasiswa.findOne({username: data.username})
+  
+      if (!user || !bcrypt.compareSync(data.password, user.password)) {
+        res.json({message: "invalid authentication"})
+      }
+  
+      let {_id, nim, nama, email, ...rest} = user
+      let token = jwt.sign({_id,nim,nama,email}, 'secret');
+      
+      res.json({
+        message: "login success",
+        token
+      })
+    } catch (error) {
+      next(error)
+    } 
+  },
+
+  regisMahasiswa: async (req, res, next) => {
+    try {
+      let data = {...req.body}
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(data.password, salt);
+      data.password = hash
+
+      const mahasiswa = await Mahasiswa.create(data)
+
+      res.json({
+        message: "success create data mahasiswa",
+        data: mahasiswa
+      })
+    } catch (error) {
+      next(error);
+    }
+  }
+}
