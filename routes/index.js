4dc40325dcf78acf978a288cc6bcb733d14a929a
auzanassdq <auzanassidqi@gmail.com>
2021-05-01 23:27:51 +0700

    add ambil matkul and pertemuan routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const mahasiswaRouter = require("./mahasiswa")
 const matkulRouter = require("./matkul")
+const ambilMatkulRouter = require("./ambilMatkul")
+const pertemuanRouter = require("./pertemuan")
+
+
 const auth = require('./auth');
 
 /* GET home page. */
@@ -15,5 +19,8 @@ router.get('/', async function(req, res, next) {
 router.use("/auth", auth)
 router.use("/mahasiswa", mahasiswaRouter)
 router.use("/matkul", matkulRouter)
+router.use("/ambil-matkul", ambilMatkulRouter)
+router.use("/pertemuan", pertemuanRouter)
+
 
 module.exports = router;
