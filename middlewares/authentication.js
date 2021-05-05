5b832a1fc24e713fc60204fbc45774fecc99c9a6
auzanassdq <auzanassidqi@gmail.com>
2021-05-05 23:26:39 +0700

    simple auth middleware

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,52 +1,20 @@
 "use strict";
 
+require("dotenv").config()
 const jwt = require("jsonwebtoken");
-const { PATIENT, PSIKIATER, ADMIN } = require("../constants/role");
-const SECRET_KEY = process.env.SECRET_KEY;
-const PatientsModel = require("../models/patients");
-const PsikiatersModel = require("../models/psikiaters");
-const AdminModel = require("../models/admin");
+const SECRET_KEY = process.env.JWT_SECRET;
 
 const authentication = async (req, res, next) => {
   try {
-    const { accesstoken } = req.headers;
-    if (!accesstoken) {
-      throw new Error("invalid token");
-    }
-    const { user_id, role } = jwt.verify(accesstoken, SECRET_KEY);
+    const authToken = req.headers.authorization;
+    if (!authToken) throw new Error("invalid token");
+    
+    const token = authToken.split(" ")[1]
+    if (!token) throw new Error("invalid token");
 
-    if (role !== PATIENT && role !== PSIKIATER && role !== ADMIN) {
-      throw new Error("JWT Malformed");
-    }
+    const user = jwt.verify(token, SECRET_KEY);
 
-    if (role === PATIENT) {
-      const patient = await PatientsModel.findById(user_id);
-
-      if (!patient) {
-        throw new Error("JWT Malformed");
-      }
-    }
-
-    if (role === PSIKIATER) {
-      const psikiater = await PsikiatersModel.findById(user_id);
-
-      if (!psikiater) {
-        throw new Error("JWT Malformed");
-      }
-    }
-
-    if (role === ADMIN) {
-      const admin = await AdminModel.findById(user_id);
-
-      if (!admin) {
-        throw new Error("JWT Malformed");
-      }
-    }
-
-    req.user = {
-      user_id,
-      role,
-    };
+    req.user = user
 
     next();
   } catch (error) {
