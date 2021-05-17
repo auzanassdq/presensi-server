582d838e864b3fee550d1f9b7eaf307670e58b9e
auzanassdq <auzanassidqi@gmail.com>
2021-05-17 20:44:39 +0700

    add auth for admin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,45 @@
 require("dotenv").config()
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {Mahasiswa} = require('../models');
+const { Mahasiswa, Dosen } = require('../models');
 
 module.exports = {
   login: async (req, res, next) => {
     try {
       let data = req.body
-      let user = await Mahasiswa.findOne({username: data.username})
-  
-      if (!user || !bcrypt.compareSync(data.password, user.password)) {
-        res.json({message: "invalid authentication"})
+      let role = "mahasiswa"
+
+      console.log(data);
+
+      if (data.username === process.env.ADMIN_USERNAME && data.password === process.env.ADMIN_PASSWORD) {
+        console.log("ADMIN");
+        let token = jwt.sign({
+          nama: process.env.ADMIN_USERNAME,
+          role: process.env.ADMIN_USERNAME,
+          _id: process.env.ADMIN_USERNAME,
+        }, process.env.JWT_SECRET);
+        res.json({
+          message: "login success",
+          userId: process.env.ADMIN_USERNAME,
+          token,
+        })
         return
       }
-  
-      let {_id, nim, nama, email, ...rest} = user
-      let token = jwt.sign({_id,nim,nama,email}, process.env.JWT_SECRET);
-      
+
+      let user = await Mahasiswa.findOne({ username: data.username })
+      if (!user) {
+        role = "dosen"
+        user = await Dosen.findOne({ username: data.username })
+      }
+
+      if (!user || !bcrypt.compareSync(data.password, user.password)) {
+        res.json({ message: "invalid authentication" })
+        return
+      }
+
+      let { _id, nama, ...rest } = user
+      let token = jwt.sign({ _id, nama, role }, process.env.JWT_SECRET);
+
       res.json({
         message: "login success",
         token,
@@ -24,12 +47,12 @@ module.exports = {
       })
     } catch (error) {
       next(error)
-    } 
+    }
   },
 
   regisMahasiswa: async (req, res, next) => {
     try {
-      let data = {...req.body}
+      let data = { ...req.body }
 
       const salt = bcrypt.genSaltSync(10);
       const hash = bcrypt.hashSync(data.password, salt);
