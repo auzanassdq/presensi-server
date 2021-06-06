a920f1df77f2e56a82e077a373df24330142c4f1
auzanassdq <auzanassidqi@gmail.com>
2021-06-06 13:19:55 +0700

    add route for check and delete ambil matkul

--- controllers/ambilMatkul.js
+++ controllers/ambilMatkul.js
@@ -1,56 +1,130 @@
-const {AmbilMatkul} = require("../models/")
+const { AmbilMatkul, Matkul, Pertemuan, Kehadiran } = require("../models/");
 
 module.exports = {
   getAllAmbilMatkul: async (req, res, next) => {
     try {
-      const ambilMatkul = await AmbilMatkul.find()
+      const ambilMatkul = await AmbilMatkul.find();
 
       res.json({
         message: "success get data",
-        data: ambilMatkul
-      })
+        data: ambilMatkul,
+      });
     } catch (error) {
-      next(error)
+      next(error);
+    }
+  },
+
+  checkAmbilMatkul: async (req, res, next) => {
+    try {
+      console.log("TESSSSSSSSSSSSSSSSSSSSSSSSSSS");
+      const data = req.query;
+      let ambilMatkul;
+
+      console.log(data);
+
+      ambilMatkul = await AmbilMatkul.findOne(data);
+      console.log("TEEES", ambilMatkul);
+
+      if (ambilMatkul) {
+        res.status(300).json({
+          message: "Matkul already registered",
+        });
+      } else {
+        res.status(200).json({
+          message: "Matkul not yet register",
+        });
+      }
+    } catch (error) {
+      next(error);
     }
   },
 
   getAmbilMatkulByID: async (req, res, next) => {
     try {
-      const ambilMatkul = await AmbilMatkul.findById(req.params.id)
+      const ambilMatkul = await AmbilMatkul.findById(req.params.id);
 
       res.json({
         message: "success get data",
-        data: ambilMatkul
-      })
+        data: ambilMatkul,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
 
   getMatkulByMahasiswa: async (req, res, next) => {
     try {
-      const ambilMatkul = await AmbilMatkul
-      .find({mahasiswa: req.params.mahasiswaId})
-      .populate("matkul")
+      const ambilMatkul = await AmbilMatkul.find({
+        mahasiswa: req.params.mahasiswaId,
+      }).populate("matkul");
 
       res.json({
         message: "success get data",
-        data: ambilMatkul
-      })
+        data: ambilMatkul,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
 
   addAmbilMatkul: async (req, res, next) => {
     try {
-      const ambilMatkul = await AmbilMatkul.create(req.body)
-      res.json({
+      const ambilMatkul = await AmbilMatkul.create(req.body);
+
+      const pertemuan = await Pertemuan.find({ matkul: req.body.matkul });
+
+      for (let i = 0; i < pertemuan.length; i++) {
+        let kehadiran = await Kehadiran.create({
+          pertemuan: pertemuan[i]._id,
+          mahasiswa: req.body.mahasiswa,
+        });
+
+        const pertemuanItem = await Pertemuan.findById(pertemuan[i]._id);
+        console.log(pertemuanItem);
+
+        pertemuanItem.kehadiran.push(kehadiran._id);
+        pertemuanItem.save();
+      }
+
+      res.status(200).json({
         message: "success join to matkul",
-        data: ambilMatkul
-      })
+        data: ambilMatkul,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
-}
+
+  removeAmbilMatkul: async (req, res, next) => {
+    try {
+      const ambilMatkul = await AmbilMatkul.findOne(req.body);
+
+      const pertemuan = await Pertemuan.find({ matkul: req.body.matkul });
+
+      for (let i = 0; i < pertemuan.length; i++) {
+        let kehadiran = await Kehadiran.findOne({
+          pertemuan: pertemuan[i]._id,
+          mahasiswa: req.body.mahasiswa,
+        });
+
+        const pertemuanItem = await Pertemuan.findById(pertemuan[i]._id);
+
+        let index = pertemuanItem.kehadiran.indexOf(kehadiran._id)
+        pertemuanItem.kehadiran.splice(index, 1)
+        
+        pertemuanItem.save();
+
+        kehadiran.delete()
+      }
+
+      ambilMatkul.delete()
+
+      res.status(200).json({
+        message: "success join to matkul",
+        data: ambilMatkul,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
--- routes/ambilMatkul.js
+++ routes/ambilMatkul.js
@@ -4,13 +4,17 @@ const router = express.Router();
 const { 
   addAmbilMatkul, 
   getAllAmbilMatkul, 
+  checkAmbilMatkul,
   getAmbilMatkulByID,
-  getMatkulByMahasiswa
+  getMatkulByMahasiswa,
+  removeAmbilMatkul
 } = require('../controllers/ambilMatkul');
 
 router.get('/', getAllAmbilMatkul);
+router.get('/check', checkAmbilMatkul);
 router.get('/:id', getAmbilMatkulByID);
 router.get('/mahasiswa/:mahasiswaId', getMatkulByMahasiswa);
 router.post('/', addAmbilMatkul);
+router.delete('/', removeAmbilMatkul);
 
 module.exports = router;
